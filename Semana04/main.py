9118010e6a0e8df060e344894465d7212ab924ed
RenzoFranco2403 <renzo.franco@utec.edu.pe>
2021-05-30 20:10:03 -0500

    Programa que da la opcion de sumar o restar dos numeros y reporta el resultado

--- Semana04/main.py
+++ Semana04/main.py
@@ -1,4 +1,10 @@
 print("Bienvenidos a programar en Python")
 a = int(input("Ingrese a: "))
 b = int(input("Ingrese b: "))
-print(a+b)
+opc = int(input("Ingrese opcion: "))
+if opc == 1:
+    c = a + b
+    print("{} + {} = {}".format(a, b, c))
+else:
+    c = a - b
+    print("{} - {} = {}".format(a, b, c))
