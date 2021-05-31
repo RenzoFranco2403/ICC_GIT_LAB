de244a80cb0047ac887ad23320caa358426b8736
RenzoFranco2403 <renzo.franco@utec.edu.pe>
2021-05-30 19:59:58 -0500

    Ejemplos de la clase (semana 2)

---
+++ Semana02/Bits_y_Logica.py
@@ -0,0 +1,23 @@
+#Manipulación de bits a la derecha
+print (14>>3)
+
+#Manipulacion de bits a la izquierda
+print(12<<2)
+print(23<<3)
+
+#Operador OR en bits
+print(6|8)
+print(19|17)
+
+#Operador AND en bits
+print(6&21)
+print(12&7)
+
+#Operador AND en bits con Variables
+x = 12
+x = x & 7
+print(x)
+
+x = 12
+x &= 7
+print(x)
