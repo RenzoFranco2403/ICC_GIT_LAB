06f413898fca163f0447aab011b8df8cab17f51b
RenzoFranco2403 <renzo.franco@utec.edu.pe>
2021-05-30 19:25:18 -0500

    14 ejercicio grupal

---
+++ Semana02/Ejercicio14.py
@@ -0,0 +1,47 @@
+print("Catorceavo Ejercicio:")
+print("5785 decimal a sistema binario")
+x = 5785
+
+bit1 = x % 2
+cociente1 = x // 2
+
+bit2 = cociente1 % 2
+cociente2 = cociente1 // 2
+
+bit3 = cociente2 % 2
+cociente3 = cociente2 // 2
+
+bit4 = cociente3 % 2
+cociente4 = cociente3 // 2
+
+bit5 = cociente4 % 2
+cociente5 = cociente4 // 2
+
+bit6 = cociente5 % 2
+cociente6 = cociente5 // 2
+
+bit7 = cociente6 % 2
+cociente7 = cociente6 // 2
+
+bit8 = cociente7 % 2
+cociente8 = cociente7 // 2
+
+bit9 = cociente8 % 2
+cociente9 = cociente8 // 2
+
+bit10 = cociente9 % 2
+cociente10 = cociente9 // 2
+
+bit11 = cociente10 % 2
+cociente11 = cociente10 // 2
+
+bit12 = cociente11 % 2
+cociente12 = cociente11 // 2
+
+bit13 = cociente12 % 2
+cociente13 = cociente12 // 2
+
+print("5785 = ", bit13, bit12, bit11, bit10, bit9, bit8, bit7, bit6, bit5,
+      bit4, bit3, bit2, bit1)
+
+print("")
