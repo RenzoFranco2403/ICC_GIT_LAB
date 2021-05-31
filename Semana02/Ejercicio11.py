e333f83497ba94f1c54e6a2559ce70286de30df6
RenzoFranco2403 <renzo.franco@utec.edu.pe>
2021-05-30 19:24:18 -0500

    11 ejercicio grupal

---
+++ Semana02/Ejercicio11.py
@@ -0,0 +1,36 @@
+print("Onceavo Ejercicio:")
+print("1023 decimal a sistema binario")
+x = 1023
+
+bit1 = x % 2
+cociente1 = x // 2
+
+bit2 = cociente1 % 2
+cociente2 = cociente1 // 2
+
+bit3 = cociente2 % 2
+cociente3 = cociente2 // 2
+
+bit4 = cociente3 % 2
+cociente4 = cociente3 // 2
+
+bit5 = cociente4 % 2
+cociente5 = cociente4 // 2
+
+bit6 = cociente5 % 2
+cociente6 = cociente5 // 2
+
+bit7 = cociente6 % 2
+cociente7 = cociente6 // 2
+
+bit8 = cociente7 % 2
+cociente8 = cociente7 // 2
+
+bit9 = cociente8 % 2
+cociente9 = cociente8 // 2
+
+bit10 = cociente9 % 2
+
+print(bit10, bit9, bit8, bit7, bit6, bit5, bit4, bit3, bit2, bit1)
+
+print("")
