158c406c98fed16242e2bc69b4ee118e475ef4fa
RenzoFranco2403 <renzo.franco@utec.edu.pe>
2021-05-30 19:20:43 -0500

    2 ejercicio grupal

---
+++ Semana02/Ejercicio2.py
@@ -0,0 +1,12 @@
+print("Segundo Ejercicio:")
+print("18 decimal a sistema octal")
+x = 18
+
+bit1 = x % 8
+cociente = x // 8
+
+bit2 = cociente % 8
+cociente1 = cociente // 8
+
+print("18 =", bit2, bit1)
+print("")
